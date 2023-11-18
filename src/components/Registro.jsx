a4aed2ed5206b1dfac35a64a99e8eb50e861ad53
FlorFuentes <flopy_200412@hotmail.com>
2023-11-18 19:59:37 -0300

    agregado evento

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -2,83 +2,105 @@ import React, {useState} from "react";
 
 const Registro= ()=>{
 
-    const [nombre, setNombre]= useState("");
-    const [apellido, setApellido] = useState("");
-    const [email, setEmail] = useState("");
-    const [tel, setTel] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirPassword, setConfirPassword] = useState("");
+  const [registro, setRegistro] = useState({
+    nombre: '',
+    apellido: '',
+    email: '',
+    telefono: '',
+    password: '',
+    confirmarPassword: '',
+  });
 
-    const handleSubmit= (e)=>{
-        e.preventDefault();
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRegistro((prevRegistro) => ({
+      ...prevRegistro,
+      [name]: value,
+    }));
+  };
+
+
+  const handleSubmit= (e)=>{
+      e.preventDefault();
+      console.log("Registro enviado:", registro);
+  }
 
     return(
         <div className="container mt-5">
         <h2 className="mb-4">Formulario de Registro</h2>
         <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="nombre">Nombre:</label>
-            <input
+          <div className="mb-3 d-flex flex-column justify-content-start">
+            <label htmlFor="nombre" className="form-label">Nombre:</label>
+            <input className="form-control"
               type="text"
+              name="nombre"
               id="nombre"
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
+              value={registro.nombre}
+              onChange={handleChange}
             />
           </div>
   
-          <div>
-            <label htmlFor="apellido">Apellido:</label>
-            <input
+          <div className="mb-3 d-flex flex-column justify-content-start">
+            <label htmlFor="apellido" className="form-label">Apellido:</label>
+            <input className="form-control"
+              name="apellido"
               type="text"
               id="apellido"
-              value={apellido}
-              onChange={(e) => setApellido(e.target.value)}
+              value={registro.apellido}
+              onChange={handleChange}
             />
           </div>
   
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
+          <div className="mb-3 d-flex flex-column justify-content-start">
+            <label htmlFor="email" className="form-label">Email:</label>
+            <input className="form-control"
               type="email"
               id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+              value={registro.email}
+              onChange={handleChange}
             />
           </div>
   
-          <div>
-            <label htmlFor="telefono">Teléfono:</label>
-            <input
+          <div className="mb-3 d-flex flex-column justify-content-start">
+            <label htmlFor="telefono" className="form-label">Teléfono:</label>
+            <input className="form-control"
               type="tel"
               id="telefono"
-              value={tel}
-              onChange={(e) => setTel(e.target.value)}
+              name="telefono"
+              value={registro.telefono}
+              onChange={handleChange}
             />
           </div>
   
-          <div>
-            <label htmlFor="password">Contraseña:</label>
-            <input
+          <div className="mb-3 d-flex flex-column justify-content-start">
+            <label htmlFor="password" className="form-label">Contraseña:</label>
+            <input className="form-control"
               type="password"
               id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+              value={registro.password}
+              onChange={handleChange}
             />
           </div>
   
-          <div>
-            <label htmlFor="confirmarPassword">Confirmar Contraseña:</label>
-            <input
+          <div className="mb-3 d-flex flex-column justify-content-start">
+            <label htmlFor="confirmarPassword" className="form-label">Confirmar Contraseña:</label>
+            <input className="form-control"
               type="password"
               id="confirmarPassword"
-              value={confirPassword}
-              onChange={(e) => setConfirPassword(e.target.value)}
+              name="confirmarPassword"
+              value={registro.confirmarPassword}
+              onChange={handleChange}
             />
           </div>
   
-          <button type="submit">Registrarse</button>
+          <button type="submit" className="registrarse">Registrarse</button>
         </form>
+        <div>
+        <h3>Información del Registro:</h3>
+        <pre>{JSON.stringify(registro, null, 2)}</pre>
+        </div>
       </div>
     )
 
